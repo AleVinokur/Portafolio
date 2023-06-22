40c6f6a54262bf4ee43852534b301e9a193b9bfc
alevinokur@gmail.com <ajvvja13@gmail.com>
2023-06-22 15:32:39 -0300

    Problemas con las animaciones de particle.js y typed.js

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,31 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import Typed from 'typed.js';
 
-
 @Component({
   selector: 'app-inicio',
   templateUrl: '../app.component.html',
   styleUrls: ['./inicio.component.css', '../app.component.css']
 })
-export class InicioComponent {
-
+export class InicioComponent implements OnInit {
   ngOnInit() {
     const typed = new Typed('.typed', {
-      strings: ['Texto de ejemplo', 'Otro texto de ejemplo'],
-      typeSpeed: 10, // Velocidad de escritura (en milisegundos)
-      backSpeed: 20, // Velocidad de retroceso (en milisegundos)
-      loop: false // Repetir la animación o detenerla después de escribir una vez
+      stringsElement: '.typed-text',
+      typeSpeed: 50,
+      backSpeed: 20,
+      loop: false
     });
-  }
-  
-
-  persona = {
-    nombre: "Alejandro Vinokur",
-    edad: 25,
-    lugar: "Ciudad Autonoma de Buenos Aires, Buenos Aires, Argentina",
-    redesSociales: {
-      linkeding: "https://www.linkedin.com/in/alejandro-vinokur-758596165/",
-      github: "https://github.com/AleVinokur",
-    }
   }
 }
